e0eeabd80c81d2d560aecf539aa14fcbd5d1a233
lSleepYMaN <136259999+lSleepYMaN@users.noreply.github.com>
2024-05-02 17:49:30 +0700

    logout to get

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -7,11 +7,12 @@ const router = express.Router()
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
 router.post('/verify', check.checkAuth, userController.verifyUser)
-router.post('/logout',userController.logoutUser)
+
 router.post('/sendnewcode',userController.sendNewCode)
 router.post('/forgetPassword',userController.forgetPass)
 router.post('/newPassword',check.checkAuth2, userController.newPassword)
 
+router.get('/logout',userController.logoutUser)
 router.get('/users',check.checkAuth, userController.getAllUsers)
 router.get('/user/:id', userController.getUserById)
 router.get('/test', userController.testSession)
